8b3fecf1dc3cd285cb20ba9af186c064e564a963
HilalSener <crescent.forest@hotmail.com>
2017-04-04 00:11:56 +0300

    EarthQ list UI fixed.

--- EarthQ/EarthQ/EarthQ/Models/EarthQListModel.cs
+++ EarthQ/EarthQ/EarthQ/Models/EarthQListModel.cs
@@ -10,11 +10,22 @@ namespace EarthQ.Models
     public class Earthquake
     {
         public string Date { get; set; }
+        public string FormattedDate
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Date))
+                {
+                    return DateTime.Parse(Date).ToString("g");
+                }
+                return string.Empty;
+            }
+        }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public double Deepth { get; set; }
         public double Md { get; set; }
-        public double Ml { get; set; }
+        public string Ml { get; set; }
         public double Mw { get; set; }
         public string Location { get; set; }
         public string Result { get; set; }
